5c22c796f3e5b4ea77481252dd813ec462242523
Igor <116892468+igor-alvs@users.noreply.github.com>
2023-05-21 20:13:01 -0300

    Add files via upload

---
+++ Tabela.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from selenium import webdriver
+import pyautogui as pg
+import time
+
+navegador = webdriver.Chrome()
+dados_excel = pd.read_csv("Clientes.csv")
+magalu = "https://www.magazineluiza.com.br/".replace('"',"")
+magalu_x = '//*[@id="input-search"]'.replace("'","")
+amazon = "https://www.amazon.com.br/ ".replace('"',"")
+amazon_x = '//*[@id="twotabsearchtextbox"]'
+
+links = [magalu,amazon]
+searchbar = [magalu_x,amazon_x]
+
+for i in range(len(links)):
+    navegador.get(f"{links[i]}")
+    botao = navegador.find_element("xpath", f"{searchbar[i]}")
+    botao.click()
+    botao.send_keys("S22 Ultra")
+    pg.press("enter")
+    time.sleep(1)
+#pg.press("win")
+
